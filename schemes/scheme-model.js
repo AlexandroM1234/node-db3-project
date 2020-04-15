47db270533f30e1d5c8f84931780ea9c052d9572
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-15 16:59:44 -0400

    scheme model coming along

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,28 @@ const db = require("../db-config");
 
 module.exports = {
   find,
+  findById,
+  findSteps,
+  add,
+  //   remove,
 };
 
 function find() {
   return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+  return db("steps").where({ id });
+}
+
+function add(newScheme) {
+  return db("schemes")
+    .insert(newScheme, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
 }
